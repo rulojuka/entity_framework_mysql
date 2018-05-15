e3d70c7ef4d7bdce04f34ccb65303192a43c927a
Alessandro Palmeira <alessandro.palmeira@gmail.com>
2018-05-14 23:01:31 -0300

    Install-package EntityFramework + BlogContext + Post + Arruma Web.config

---
+++ BlogEntity/BlogEntity/Infra/BlogContext.cs
@@ -0,0 +1,20 @@
+﻿using BlogEntity.Models;
+using MySql.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BlogEntity.Infra
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    public class BlogContext : DbContext
+    {
+        public BlogContext() : base("name=BlogContext")
+        {
+
+        }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ BlogEntity/BlogEntity/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogEntity.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Resumo { get; set; }
+        public string Categoria { get; set; }
+    }
+}
